8b9edb130f734522c0928a53942b98d3e2ec216f
Hemant142 <yhemant203@gmail.com>
2023-09-26 23:06:07 +0530

    Sign Up srtucture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
      <h1>NEM111</h1>
+     <AllRoutes/>
     </div>
   );
 }
