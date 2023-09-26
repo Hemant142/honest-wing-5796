83f66df2e3e9509d6c70616b4211062f36aff339
Hemant142 <yhemant203@gmail.com>
2023-09-26 13:13:05 +0530

    Backend

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+
+const express=require("express");
+require("dotenv").config()
+const cors=require("cors");
+const {connection}=require("./db")
+const app=express();
+app.use(express.json())
+app.use(cors())
+
+app.get("/",async(req,res)=>{
+    res.setHeader("Content-type", "text/html")
+    res.send("<h1>Welcome to the Sare Gama server</h1>")
+})
+
+const PORT=process.env.PORT
+app.listen(PORT,async()=>{
+ try{
+await connection
+console.log("You are connected to Saregama");
+console.log(`Server is running on Port: ${PORT}`)
+ }catch(err){
+    console.log(err.message)
+ }
+})
